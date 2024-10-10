f4d586225481c5cdca283ae6ed4153626d3a814b
Petr Sebik <sebikpetr@gmail.com>
2024-10-10 22:20:42 +0200

    removed commented code

--- components/card/card.py
+++ components/card/card.py
@@ -1,13 +1,7 @@
-from typing import Dict, Any
-from django_components import component  # type: ignore
+from django_components import component
 
 
 @component.register("card")
 class Card(component.Component):
     template_name = "card/card.html"
 
-    # def get_context_data(self, *args, **kwargs) -> Dict[str, Any]:
-    #     return {
-    #         "args": args,
-    #         "kwargs": kwargs,
-    #     }
