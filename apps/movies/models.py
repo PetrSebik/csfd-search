cb866ab50fa333261a9d08389b04b1d55ec6c6ed
Petr Sebik <sebikpetr@gmail.com>
2024-10-11 15:34:12 +0200

    indexes on filter fields

--- apps/movies/models.py
+++ apps/movies/models.py
@@ -4,7 +4,7 @@ from apps.movies.utils import remove_accents
 
 class Movie(models.Model):
     name = models.CharField(max_length=128)
-    name_unaccented = models.CharField(max_length=128)
+    name_unaccented = models.CharField(max_length=128, db_index=True)
     actors = models.ManyToManyField("Actor", related_name="movies")
 
     def save(self, *args, **kwargs):
@@ -14,7 +14,7 @@ class Movie(models.Model):
 
 class Actor(models.Model):
     name = models.CharField(max_length=128)
-    name_unaccented = models.CharField(max_length=128)
+    name_unaccented = models.CharField(max_length=128, db_index=True)
 
     def save(self, *args, **kwargs):
         self.name_unaccented = remove_accents(self.name)
